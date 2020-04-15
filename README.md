5eeb5e612308227b18ed30d4a7fb15e64e556ed6
Jake <mcswain44@gmail.com>
2020-04-15 11:20:55 -0400

    Completed project with functional calculator. It adds divides multiplys and subtracts. There are some known issues.

---
+++ README.md
@@ -0,0 +1,8 @@
+This project is part of The Odin Project's online curriculum. The idea of the project is simple, to create an online calculator that behaves as one would expect a normal calculator to. 
+
+I will start by getting basic functions and the display built and then will tweak by adding style and additional functionality. 
+
+Results:
+
+This project was a challenge. I did habe to rely on some other students solutions. Special credit to rueeazy! Using css html and js I was able to get this up and running and styled to the point that I felt good about it. 
+
---
+++ script.js
@@ -0,0 +1,91 @@
+// This script contains all of the functions for an online calculator **Written By Jake McSwain//
+
+//Const for buttons and locations//
+const numbers = document.querySelectorAll('.numbers');
+const operations = document.querySelectorAll('.functions');
+const display = document.getElementById('output');
+const input = display.innerHTML;
+const clearButton = document.getElementById('clear');
+const equalButton = document.getElementById('equals')
+let currentNum = ""
+let previousNum = ""
+let error = "Error"
+let operator = undefined;
+
+//Event Listeners for Clicks on Buttons//
+numbers.forEach(button => button.addEventListener('click', () => {
+    appendNumber(button.innerText)
+    updateDisplay();
+})); 
+
+operations.forEach(button => button.addEventListener('click', () => {
+    chooseOperation(button.innerText)
+}));
+
+equalButton.addEventListener('click', () => {
+    operate();
+    updateDisplay();
+});
+    
+clearButton.addEventListener('click', () => {
+    clear();
+    updateDisplay();
+});
+
+
+
+//function to run everything//
+
+function updateDisplay () {
+    display.textContent = currentNum;
+}
+
+function appendNumber (number) {
+    if (number === '.' && currentNum.includes('.')) return;
+    currentNum = currentNum.toString() + number.toString();
+}
+
+function chooseOperation(operation) {
+    if (currentNum === '') return;
+    if (previousNum !== '') {
+        operate();
+        updateDisplay();
+    }
+    operator = operation;
+    previousNum = currentNum;
+    currentNum = '';
+}
+
+function operate() {
+    let result;
+    let prev = parseFloat(previousNum);
+    let curr = parseFloat(currentNum);
+    switch (operator) {
+        case '+':
+            result = prev + curr;
+            break;
+        case '-': 
+            result = prev - curr;
+            break;
+        case '*':
+            result = prev * curr;
+            break;
+        case '÷':
+            if (curr === 0) {
+                result = error;
+            } else result = prev / curr;
+            break;
+        default :
+        return;
+    }
+    
+    currentNum = result;
+    operation = undefined;
+    previousNum = '';
+}
+
+function clear() {
+    operator = undefined;
+    previousNum = '';
+    currentNum = '';
+}
